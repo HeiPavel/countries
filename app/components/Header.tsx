0b846bf1ba320e0a8bd770fbb9968db7952496a6
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2025-01-24 01:31:27 +0200

    Add Theme Switch

---
+++ app/components/Header.tsx
@@ -0,0 +1,12 @@
+import { ThemeSwitch } from './ThemeSwitch'
+
+export function Header() {
+  return (
+    <header className='dark:bg-grey-light bg-white-light px-4 tablet:px-10 desktop:px-20'>
+      <div className='container h-24 tablet:h-20 flex justify-between items-center'>
+        <p className='text-lg tablet:text-xl laptop:text-2xl font-bold dark:text-white-light text-black'>Where in the world?</p>
+        <ThemeSwitch/>
+      </div>
+    </header>
+  )
+}
