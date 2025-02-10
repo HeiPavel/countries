a4540852131e4740da2c4f3ed954da980d4fdfc8
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2025-02-10 03:46:18 +0200

    Add fetching data for country page

--- app/country/[code]/page.tsx
+++ app/country/[code]/page.tsx
@@ -1,9 +1,72 @@
+import { CountryRaw, NameRaw } from '@/app/page'
+
 type Params = {
   params: Promise<{code: string}>
 }
 
+type Currency = {
+  name: string
+  symbol: string
+}
+
+type Currencies = {
+  [key: string]: Currency
+}
+
+type Languages = {
+  [key: string]: string
+}
+
+interface CountryFullPreviewRaw extends Omit<CountryRaw, 'altSpellings' | 'cca3'> {
+  subregion: string
+  tld: string[]
+  currencies: Currencies
+  languages: Languages
+  borders: string[]
+}
+
+interface CountryFullPreview extends Omit<CountryFullPreviewRaw, 'name' | 'currencies' | 'languages'> {
+  name: string
+  nativeName: string
+  currencies: string[]
+  languages: string[]
+}
+
+type BorderCountryRaw = {
+  name: NameRaw
+  cca3: string
+}
+
+type BorderCountry = {
+  name: string
+  cca3: string
+}
+
 export default async function CountryPage({params}: Params) {
-  const code = (await params).code
+  const code = (await params).code.toUpperCase()
+
+  const countryDataRaw: CountryFullPreviewRaw[] = await fetch(`https://restcountries.com/v3.1/alpha?codes=${code}&fields=name,population,region,subregion,capital,tld,currencies,languages,borders,flags`).then(res => res.json())
+  const {name, currencies, languages, borders, ...rest} = countryDataRaw[0]
+  const nativeNames = Object.values(name.nativeName)
+  const currenciesToPreview = Object.values(currencies).map(currency => currency.name)
+  const languagesToPreview = Object.values(languages)
+  
+  const countryData: CountryFullPreview = {
+    name: name.official,
+    nativeName: nativeNames.length ? nativeNames[0].official : name.official,
+    borders: borders.map(code => code.toUpperCase()),
+    currencies: currenciesToPreview,
+    languages: languagesToPreview,
+    ...rest
+  }
+  
+  const borderCountriesDataRaw: BorderCountryRaw[] = countryData.borders.length ? await fetch(`https://restcountries.com/v3.1/alpha?codes=${countryData.borders.join(',')}&fields=name,cca3`).then(res => res.json()) : []
+  const borderCountriesData: BorderCountry[] = borderCountriesDataRaw.map(contry => {
+    return {
+      name: contry.name.common,
+      cca3: contry.cca3
+    }
+  })
   
   return (
     <h1>{code}</h1>
